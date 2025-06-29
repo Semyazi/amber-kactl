15e5f2c63f780e50b8ab62941e12e5879eb75b42
Semyazi <34780338+Semyazi@users.noreply.github.com>
2025-06-29 16:25:27 -0400

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -7,11 +7,17 @@ using namespace std;
 #define rev(i, a, b) for(int i = (b); i >= (a); --i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
+#define int long long
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-int main() {
+void solve(){
+
+}
+
+int32_t main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
+	solve();
 }
