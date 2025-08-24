eee8e3787c934042b06545e8d5aecdb71b3f1798
Evan Simanovskis <evan.simanovskis@gmail.com>
2025-08-24 14:06:43 -0400

    added new segment trees

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,83 @@
 /**
- * Author: Simon Lindholm
- * Date: 2016-10-08
- * License: CC0
- * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
+ * Author: Andrew Tam
+ * Date: 2025-8-11
+ * Description: Lazy segment tree that supports adding and setting intervals.
  * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
  * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
+ * Status: trusted
  */
 #pragma once
 
-#include "../various/BumpAllocator.h"
+template<typename Data,typename Aux>
+class LazySegmentTree{
+private:
+	int n;
+	vector<Data>t;
+	vector<Aux>lz;
+	void build(int L,int R,int v,const vector<Data>&a){
+		if(L==R){
+			t[v]=a[L];return;
+		}
+		int M=(L+R)/2;
+		build(L,M,2*v,a);
+		build(M+1,R,2*v+1,a);
+		t[v]=Data::merge(t[2*v],t[2*v+1]);
+	}
+	void push(int v,int len){
+		lz[v].apply(t[2*v],lz[2*v],(len+1)/2);
+		lz[v].apply(t[2*v+1],lz[2*v+1],len/2);
+		lz[v]=Aux();
+	}
+	void update(int qL,int qR,const Aux&val,int L,int R,int v){
+		if(qL<=L && R<=qR){
+			val.apply(t[v],lz[v],R-L+1);return;
+		}
+		if(qR<L || R<qL)return;
+		push(v,R-L+1);
+		int M=(L+R)/2;
+		update(qL,qR,val,L,M,2*v);
+		update(qL,qR,val,M+1,R,2*v+1);
+		t[v]=Data::merge(t[2*v],t[2*v+1]);
+	}
+	Data query(int qL,int qR,int L,int R,int v){
+		if(qL<=L && R<=qR)return t[v];
+		if(qR<L || R<qL)return Data();
+		push(v,R-L+1);
+		int M=(L+R)/2;
+		return Data::merge(query(qL,qR,L,M,2*v),query(qL,qR,M+1,R,2*v+1));
+	}
+public:
+	LazySegmentTree(int n):n(n){
+		t.resize(4*n);lz.resize(4*n);
+	}LazySegmentTree(int n,const vector<Data>&a):n(n){
+		t.resize(4*n);lz.resize(4*n);
+		build(1,n,1,a);
+	}void update(int qL,int qR,const Aux&val){update(qL,qR,val,1,n,1);}
+	Data query(int qL,int qR){return query(qL,qR,1,n,1);}
+};
 
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
+struct Data{
+	int v;
+	Data(int v):v(v){}
+	Data():v(0){}
+	static Data merge(const Data&data1,const Data&data2){
+		return Data(data1.v+data2.v);
+	};
+};
+
+struct Aux{
+	int add,set;
+	Aux(int add,int set):add(add),set(set){}
+	Aux():add(0),set(-1){}
+	void apply(Data&data,Aux&aux,int len) const{
+		if(set!=-1){
+			data.v=len*set;
+			aux.set=set;
+			aux.add=0;
+			return;
 		}
-		else val = v[lo];
-	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
-		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
+		data.v+=len*add;
+		if(aux.set!=-1)aux.set+=add;
+		else aux.add+=add;
 	}
 };
--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -1,31 +1,57 @@
 /**
- * Author: Lucian Bicsi
- * Date: 2017-10-31
- * License: CC0
- * Source: folklore
- * Description: Zero-indexed max-tree. Bounds are inclusive to the left and exclusive to the right.
- * Can be changed by modifying T, f and unit.
- * Time: O(\log N)
- * Status: stress-tested
+ * Author: Andrew Tam
+ * Date: 2025-8-11
+ * Description: Minimum segment tree.
+ * Can be changed to other things.
+ * Time: O(\log N).
+ * Status: trusted
  */
 #pragma once
 
-struct Tree {
-	typedef int T;
-	static constexpr T unit = INT_MIN;
-	T f(T a, T b) { return max(a, b); } // (any associative fn)
-	vector<T> s; int n;
-	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
-	void update(int pos, T val) {
-		for (s[pos += n] = val; pos /= 2;)
-			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
-	}
-	T query(int b, int e) { // query [b, e)
-		T ra = unit, rb = unit;
-		for (b += n, e += n; b < e; b /= 2, e /= 2) {
-			if (b % 2) ra = f(ra, s[b++]);
-			if (e % 2) rb = f(s[--e], rb);
+template<typename Data>
+class SegmentTree{
+private:
+	int n;
+	vector<Data>t;
+	void build(int L,int R,int v,const vector<Data>&a){
+		if(L==R){
+			t[v]=a[L];return;
 		}
-		return f(ra, rb);
+		int M=(L+R)/2;
+		build(L,M,2*v,a);
+		build(M+1,R,2*v+1,a);
+		t[v]=Data::merge(t[2*v],t[2*v+1]);
 	}
+	void update(int qI,const Data&val,int L,int R,int v){
+		if(L==R){
+			t[v]=val;return;
+		}
+		int M=(L+R)/2;
+		if(qI<=M)update(qI,val,L,M,2*v);
+		else update(qI,val,M+1,R,2*v+1);
+		t[v]=Data::merge(t[2*v],t[2*v+1]);
+	}
+	Data query(int qL,int qR,int L,int R,int v){
+		if(qL<=L && R<=qR)return t[v];
+		if(qR<L || R<qL)return Data();
+		int M=(L+R)/2;
+		return Data::merge(query(qL,qR,L,M,2*v),query(qL,qR,M+1,R,2*v+1));
+	}
+public:
+	SegmentTree(int n):n(n){
+		t.resize(4*n);
+	}SegmentTree(int n,const vector<Data>&a):n(n){
+		t.resize(4*n);
+		build(1,n,1,a);
+	}void update(int qI,const Data&val){update(qI,val,1,n,1);}
+	Data query(int qL,int qR){return query(qL,qR,1,n,1);}
+};
+
+struct Data{
+	int v;
+	Data(int v):v(v){}
+	Data():v(INF){}
+	static Data merge(const Data&data1,const Data&data2){
+		return Data(min(data1.v,data2.v));
+	};
 };
