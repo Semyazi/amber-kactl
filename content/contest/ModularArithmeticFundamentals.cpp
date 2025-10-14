0310e1b136a29306ae2dd5f2d6c8882c806735ec
Evan Simanovskis <evan.simanovskis@gmail.com>
2025-10-14 18:31:08 -0400

    hi

--- content/contest/ModularArithmeticFundamentals.cpp
+++ content/contest/ModularArithmeticFundamentals.cpp
@@ -10,3 +10,17 @@ int powmod(int b,int p,int md){
 int invert(int x){
     return powmod(x,mod-2,mod);
 }
+const int F = 2000001;
+int fac[F];
+int ifac[F];
+void setup(){
+	fac[0]=1;
+	rep(i,1,F)fac[i]=(i*fac[i-1])%mod;
+	ifac[F-1]=invert(fac[F-1]);
+	rev(i,1,F-1)ifac[i-1]=(i*ifac[i])%mod;
+}
+int comb(int n,int k){
+	if(n<0 || k>n)return 0;
+	int x=(fac[n]*ifac[k])%mod;
+	return(x*ifac[n-k])%mod;
+}
